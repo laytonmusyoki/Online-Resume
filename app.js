8096e0a9ffc47b079d15590262fc3fe13da9afc4
laytonmusyoki <laytonmatheka7@gmail.com>
2023-07-22 10:11:41 +0300

    resume

---
+++ app.js
@@ -0,0 +1,17 @@
+var change=document.getElementById('change');
+
+
+    mytimeout=setInterval(function(){
+        if(change.src="kali2.jpg"){
+            change.src="img2.jpg";
+        }
+        else{
+            change.src="kal2.jpg";
+        }
+    },5000);
+    
+
+
+
+   
+ 
